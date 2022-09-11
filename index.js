17c656b596ca85e6c14249477e4462c5efde18b0
ariyan rumi <58899858+Ariyan550@users.noreply.github.com>
2022-09-11 20:52:49 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const link =document.querySelectorAll('a')
+const btn =document.querySelector('.re')
+link.forEach((curEle)=>{
+   curEle.addEventListener("click",()=>{
+    let pass =window.prompt('Enter Your Password')
+    let dpass =1010
+    if(pass!=dpass){
+        curEle.href='Wrong Password'
+    }else{
+        curEle.href=curEle
+    }
+   })
+
+})
+
+btn.addEventListener('click',()=>{
+    location.reload();
+})
