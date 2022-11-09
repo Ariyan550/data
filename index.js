641303a93464b80650b070b4e2b538fb7d6bf5a6
ariyan rumi <58899858+Ariyan550@users.noreply.github.com>
2022-11-09 10:07:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const btn =document.querySelector('.re')
 link.forEach((curEle)=>{
    curEle.addEventListener("click",()=>{
     let pass =window.prompt('Enter Your Password')
-    let dpass =1010
+    let dpass =10105
     if(pass!=dpass){
         curEle.href='Wrong Password'
     }else{
